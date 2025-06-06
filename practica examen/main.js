0f72dc832927d864384cf68dd8f3ffa116eb4558
miaa-sabella <miaisa1x@gmail.com>
2025-06-06 15:54:05 -0600

    practica de examen

---
+++ practica examen/main.js
@@ -0,0 +1,23 @@
+    
+fetch('estudiantes.json')
+.then(res => res.json())
+.then((obtenerdatos) => {
+    datosJson = obtenerdatos;
+});
+
+let names = document.getElementById("nombres");
+function registrarNota(nombre){
+    
+    let respuesta = parseInt(prompt("cual es tú nota?"));
+    if (respuesta >= 0 && respuesta <= 100){
+        console.log(respuesta);
+    }
+    else{
+        alert("por favor inserte su nota");
+    };
+
+}
+
+function registrarnota(){
+
+}
